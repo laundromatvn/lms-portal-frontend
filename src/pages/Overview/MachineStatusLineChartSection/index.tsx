67a9f677d0f60d2bdc5304f643a42ba06d704643
Ryan <nguyenduylan711@gmail.com>
2025-10-21 21:51:15 +0700

    add controller selection

--- src/pages/Overview/MachineStatusLineChartSection/index.tsx
+++ src/pages/Overview/MachineStatusLineChartSection/index.tsx
@@ -1,13 +1,16 @@
 import React, { use, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Empty, Flex, Select, Skeleton, Typography } from 'antd';
+import { Button, Empty, Flex, Select, Skeleton, Typography } from 'antd';
+
+import { Refresh } from '@solar-icons/react';
 
 import { useTheme } from '@shared/theme/useTheme';
 
 import { tenantStorage } from '@core/storage/tenantStorage';
 
 import { useListStoreApi, type ListStoreResponse } from '@shared/hooks/useListStoreApi';
+import { useListControllerApi, type ListControllerResponse } from '@shared/hooks/useListControllerApi';
 import { useListMachineApi, type ListMachineResponse } from '@shared/hooks/useListMachineApi';
 import { useGetMachineStatusLineChartApi } from '@shared/hooks/dashboard/useGetMachineStatusLineChartApi';
 
@@ -26,6 +29,7 @@ export const MachineStatusLineChartSection: React.FC<Props> = ({ height }) => {
 
   const tenant = tenantStorage.load();
   const [selectedStoreId, setSelectedStoreId] = useState<string | undefined>(undefined);
+  const [selectedControllerId, setSelectedControllerId] = useState<string | undefined>(undefined);
   const [selectedMachineId, setSelectedMachineId] = useState<string | undefined>(undefined);
 
   const {
@@ -33,14 +37,18 @@ export const MachineStatusLineChartSection: React.FC<Props> = ({ height }) => {
     data: machineStatusLineChart,
     loading: machineStatusLineChartLoading,
   } = useGetMachineStatusLineChartApi();
-
   const {
     listStore,
     data: stores,
     loading: storesLoading,
     error: storesError,
   } = useListStoreApi<ListStoreResponse>();
-
+  const {
+    listController,
+    data: controllers,
+    loading: controllersLoading,
+    error: controllersError,
+  } = useListControllerApi<ListControllerResponse>();
   const {
     listMachine,
     data: machines,
@@ -48,24 +56,28 @@ export const MachineStatusLineChartSection: React.FC<Props> = ({ height }) => {
     error: machinesError,
   } = useListMachineApi<ListMachineResponse>();
 
-  useEffect(() => {
-    if (selectedStoreId && selectedMachineId) {
-      getMachineStatusLineChart({
-        store_id: selectedStoreId as string,
-        machine_id: selectedMachineId as string
-      });
-    }
-  }, [selectedStoreId, selectedMachineId]);
+  const handleListOverviewOrder = async () => {
+    if (!selectedMachineId) return;
+
+    getMachineStatusLineChart({
+      store_id: selectedStoreId as string,
+      machine_id: selectedMachineId as string
+    });
+  };
 
   useEffect(() => {
-    if (machines) {
-      console.log(machines);
+    handleListOverviewOrder();
+  }, [selectedMachineId]);
+
+  useEffect(() => {
+    if (selectedControllerId) {
+      listMachine({ controller_id: selectedControllerId as string, page: 1, page_size: 100 });
     }
-  }, [selectedStoreId, selectedMachineId]);
+  }, [selectedControllerId]);
 
   useEffect(() => {
     if (selectedStoreId) {
-      listMachine({ store_id: selectedStoreId as string, page: 1, page_size: 100 });
+      listController({ store_id: selectedStoreId as string, page: 1, page_size: 100 });
     }
   }, [selectedStoreId]);
 
@@ -82,10 +94,15 @@ export const MachineStatusLineChartSection: React.FC<Props> = ({ height }) => {
       <LeftRightSection
         left={null}
         right={(
-          <Flex gap={theme.custom.spacing.medium}>
+          <Flex align="center" gap={theme.custom.spacing.medium}>
+            <Button
+              icon={<Refresh />}
+              onClick={handleListOverviewOrder}
+            />
+
             <Select
               options={machines?.data.map((machine) => ({
-                label: machine.name,
+                label: machine.name ? `Relay ${machine.relay_no} - ${machine.name}` : `Relay ${machine.relay_no}`,
                 value: machine.id,
               }))}
               value={selectedMachineId}
@@ -94,7 +111,34 @@ export const MachineStatusLineChartSection: React.FC<Props> = ({ height }) => {
               }}
               loading={storesLoading}
               placeholder={t('common.selectMachine')}
-              style={{ width: 240 }}
+              style={{ width: 180 }}
+            />
+
+            <Select
+              options={controllers?.data.map((controller) => ({
+                label: controller.name
+                  ? <Flex vertical gap={theme.custom.spacing.small}>
+                    <Typography.Text>{controller.name}</Typography.Text>
+                    <Typography.Text type="secondary">{controller.device_id}</Typography.Text>
+                  </Flex>
+                  : <Flex gap={theme.custom.spacing.small}>
+                    <Typography.Text type="secondary">{t('common.controller')}</Typography.Text>
+                    <Typography.Text type="secondary">{controller.device_id}</Typography.Text>
+                  </Flex>,
+                value: controller.id,
+                render: (_: any, record: any) => record.name ? `${record.name} (${record.device_id})` : record.device_id,
+              }))}
+              value={selectedControllerId}
+              onChange={(value) => {
+                setSelectedControllerId(value);
+              }}
+              labelRender={(label) => {
+                const selectedController = controllers?.data.find(controller => controller.id === selectedControllerId);
+                return selectedController?.name || selectedController?.device_id || label?.label;
+              }}
+              loading={controllersLoading}
+              placeholder={t('common.selectController')}
+              style={{ width: 180 }}
             />
 
             <Select
@@ -108,7 +152,7 @@ export const MachineStatusLineChartSection: React.FC<Props> = ({ height }) => {
               }}
               loading={storesLoading}
               placeholder={t('common.selectStore')}
-              style={{ width: 240 }}
+              style={{ width: 180 }}
             />
           </Flex>
         )}
